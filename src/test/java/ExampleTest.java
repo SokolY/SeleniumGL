a20a774501cb990b82e4e56a08f66a430f8efd8a
sokoly <yura.sokol@gmail.com>
2021-06-20 12:19:20 +0300

    MenuCheckTask

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,13 +1,22 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExampleTest {
 
     static WebDriver driver;
-
+    WebElement userName;
+    WebElement password;//
+    WebElement lgnBtn;
+    WebElement menuItem;
+    WebElement menuHeader;
 
     @BeforeAll
     public static void beforeAll() {
@@ -20,10 +29,32 @@ public class ExampleTest {
     }
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws InterruptedException {
         System.out.println("Before Each Test");
+        driver.get("http://158.101.173.161/admin/login.php?redirect_url=%2Fadmin%2F");
+        Thread.sleep(3000);
+        WebElement userName = driver.findElement(By.name("username"));
+        WebElement password = driver.findElement(By.name("password"));//
+        WebElement lgnBtn = driver.findElement(By.cssSelector(".btn-default"));
+        userName.sendKeys("testadmin");
+        password.sendKeys("R8MRDAYT_test");
+        Thread.sleep(2000);
+        lgnBtn.click();
+
     }
 
+    @Test
+    public void prt() throws InterruptedException {
+        Thread.sleep(2000);
+        ArrayList<WebElement> menuItems = new ArrayList<WebElement>(driver.findElements(By.cssSelector("#box-apps-menu>li")));
+        System.out.println("List length - " + menuItems.size());
+        for (int i = 0; i<menuItems.size(); i++){
+            menuItem = menuItems.get(i);
+            menuItem.click();
+            Thread.sleep(2000);
+        }
+
+    }
     @AfterAll
     public static void afterAll() {
         System.out.println("After Test");
@@ -31,18 +62,15 @@ public class ExampleTest {
 
     @AfterEach
     public void afterEach() {
-        System.out.println("After Each Test");
-    }
-
-        @Test
-    public void prt(){
-
-        driver.get("https://google.com");
         driver.close();
+        System.out.println("After Each Test");
+
     }
 
-    @Test
-    public void prt2(){
-        System.out.println("Hello World2");
-    }
+
+
+//    @Test
+//    public void prt2(){
+//        System.out.println("Hello World2");
+//    }
 }
